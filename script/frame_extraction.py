4e3e6445fdbf533cee779cadc2e3a63b58f50101
woven-visionai <157009640+woven-visionai@users.noreply.github.com>
2024-01-19 21:25:42 +0900

    Create frame_extraction.py

---
+++ script/frame_extraction.py
@@ -0,0 +1,17 @@
+import cv2
+
+def processing(video_path):
+  
+  vidcap = cv2.VideoCapture(video_path)
+  readable,frame = vidcap.read()
+  count = 0
+  
+  while readable:
+    cv2.imwrite("frame%d.jpg" % count, image) 
+    readable,image = vidcap.read()
+    count += 1
+        
+if __name__ == '__main__':
+    
+    video_path = 'path/to/../20230707_12_SN17_T1_Camera1_0.mp4'
+    processing(video_path)
