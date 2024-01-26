95ebcb56cfb1c4ec145bcd10d6e9d9a5cf47e016
woven-visionai <157009640+woven-visionai@users.noreply.github.com>
2024-01-26 20:47:15 +0900

    Update frame_extraction.py

--- script/frame_extraction.py
+++ script/frame_extraction.py
@@ -1,13 +1,18 @@
-import cv2
+import cv2 
 
 def processing(video_path):
   
   vidcap = cv2.VideoCapture(video_path)
+  fps = vidcap.get(cv2.CAP_PROP_FPS)
+
   readable,image = vidcap.read()
   count = 0
-  
+
   while readable:
-    cv2.imwrite("frame%d.jpg" % count, image) 
+    cv2.imwrite("frame%d.jpg" % count, image)
+    if fps == 60:
+      count += 1 
+      cv2.imwrite("frame%d.jpg" % count, image)
     readable,image = vidcap.read()
     count += 1
         
